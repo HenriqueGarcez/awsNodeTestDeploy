f75459d4178f7157f4bb7cf8ad4969d2eb91085f
henrique_conti <henrique.garcez@compasso.com.br>
2021-02-10 07:44:40 -0300

    Fix: Sample Node App

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,3 +12,6 @@ sudo apt-get install nodejs -y
 # Install forever module 
 # https://www.npmjs.com/package/forever
 sudo npm install forever -g
+
+# Clean working folder
+# sudo find /home/ubuntu/test -type f -delete
